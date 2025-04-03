adc5b0f57188d0e63d02b49b5f71d6f499679b01
Kevin Araujo <kevin_santana.araujo@hotmail.com>
2025-04-03 16:11:01 -0300

    lend challenge v4

--- interviews/lend/api_integration.py
+++ interviews/lend/api_integration.py
@@ -13,14 +13,14 @@ from constants import DATE_FORMAT, DEV_ENVS, URL
 
 class Config(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+        env_file=".env", extra="ignore", env_file_encoding="utf-8"
     )
     api_key: str = ""
-    env: str = ""
+    env: str = "dev"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if os.getenv("ENV", "dev") not in DEV_ENVS:
+        if os.getenv("env", "dev") not in DEV_ENVS:
             load_dotenv(dotenv_path=None, override=True)
 
 
@@ -29,54 +29,63 @@ config = Config()
 
 def main(n_days: int, city_code: str):
     try:
-        url = URL.format(n_days=n_days, city_code=city_code, api_key=config.api_key)
         logger.info(
-            f"Fetching data from weather API. With days {n_days}, city {city_code} and URL {URL}"
+            f"Start fetching data from weather API. Days {n_days}, City {city_code}, URL {URL}"
         )
-        forecasts = requests.get(url)
+        url = URL.format(n_days=n_days, city_code=city_code, api_key=config.api_key)
+        forecasts = requests.get(url=url)
         if forecasts.status_code != 200:
-            logger.exception("Exception while fetching data from weather API.")
-            raise Exception(
-                "Unexpected error while fetching data from weather API."
-                f"Code {forecasts.status_code} Body {forecasts.text}"
-            )
+            text = "Unexpected error occured in main func. "
+            f"Status code {forecasts.status_code}, Response {forecasts.text}"
+            logger.exception(text)
+            raise Exception(text)
+
         forecasts = forecasts.json()
         weather_forecast = {"forecasts": []}
-
         for day in forecasts["DailyForecasts"]:
-            logger.info(f"Fetching day {day['Date']} weather data")
-            day_formatted = _format_date(day["Date"])
+            date_formatted = _format_date(day["Date"])
+            logger.info(f"Fetching weather data on day {date_formatted}")
             max_temp = day["Temperature"]["Maximum"]["Value"]
-            max_temp_cloth = _max_temp_cloth(max_temp)
-            logger.info(f"Cloth for maximum temperature {max_temp}: {max_temp_cloth}")
-            weather_prob = _get_prob_from_weather(day)
-            cloth_for_weather = _cloth_for_weather(*weather_prob)
-            logger.info(
-                f"Cloth for weather {weather_prob[1]} with prob {weather_prob[0]}"
-            )
+            max_temp_cloth = _max_temp_clothing(max_temp)
+            weather_prob = _get_highest_prob_weather(day)
+            weather_clothing = _clothing_from_weather(*weather_prob)
             forecast = _format_response(
-                day_formatted, max_temp_cloth, cloth_for_weather
+                date=date_formatted,
+                max_temp_cloth=max_temp_cloth,
+                weather_clothing=weather_clothing,
             )
             weather_forecast["forecasts"].append(forecast)
+            logger.info(
+                f"Fetched weather clothing. Max temp {max_temp}, Max temp clothing {max_temp_cloth}, "
+                f"Weather probability {weather_prob}, Weather clothing {weather_clothing}"
+            )
 
         return weather_forecast
 
     except Exception as e:
-        logger.exception(f"An unexcepted exception occured: {str(e)}")
-        raise Exception
+        text = f"Unexpected error on main func: {str(e)}"
+        logger.exception(text)
+        raise Exception(text)
 
 
-def get_arg_parser():
+def get_parse_args() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--n-days", type=int, default=5, help="", dest="n_days")
+    parser.add_argument(
+        "-n",
+        "--n-days",
+        help="Number of days to fetch from the weather API.",
+        default=5,
+        type=int,
+        dest="n_days",
+    )
     parser.add_argument(
         "-c",
         "--city-code",
-        type=str,
-        dest="city_code",
-        choices=["60449", "45881", "349727", "101924", "127164"],
-        help="",
         default="45881",
+        dest="city_code",
+        type=str,
+        help="City code to fetch from weather API. Options are "
+        "60449 (Santiago), 45881 (São Paulo), 349727 (New York), 101924 (Beijing), 127164 (Cairo).",
     )
     return parser
 
@@ -84,68 +93,80 @@ def get_arg_parser():
 def _format_date(date: str) -> str:
     try:
         datetime_obj = datetime.fromisoformat(date)
-        formatted_date = datetime_obj.strftime(DATE_FORMAT)
-        return formatted_date
+        dt_formatted = datetime_obj.strftime(DATE_FORMAT)
+        return dt_formatted
+
     except Exception as e:
-        logger.exception(f"Unexpected error on _format_date: {str(e)}")
-        raise Exception(f"Unexpected error on _format_date: {str(e)}")
+        text = f"Unexpected error occured in _format_date: {str(e)}"
+        logger.exception(text)
+        raise Exception(text)
 
 
-def _max_temp_cloth(temp: int) -> Optional[Union[str, Tuple[str, str]]]:
-    if temp < 45:
-        return "Coat", "Winter jacket"
+def _max_temp_clothing(temp: int) -> Optional[Union[str, Tuple[str, str]]]:
+    try:
+        if temp < 45:
+            return "Coat", "Winter Jacket"
+        if temp >= 45 and temp <= 79:
+            return "Fleece", "Short Sleeves"
+        if temp >= 80:
+            return "Shorts"
 
-    if temp >= 45 and temp <= 79:
-        return "Fleece", "Short Sleeves"
-
-    if temp >= 80:
-        return "Shorts"
-
-
-def _get_prob_from_weather(day: Dict[str, str]) -> Tuple[int, str]:
-    day_rain_prob = day["Day"]["RainProbability"]
-    night_rain_prob = day["Night"]["RainProbability"]
-    day_snow_prob = day["Day"]["SnowProbability"]
-    night_snow_prob = day["Night"]["SnowProbability"]
-    day_ice_prob = day["Day"]["IceProbability"]
-    night_ice_prob = day["Night"]["IceProbability"]
-    probabilities = [
-        (max(day_rain_prob, night_rain_prob), "rain"),
-        (max(day_snow_prob, night_snow_prob), "snow"),
-        (max(day_ice_prob, night_ice_prob), "ice"),
-    ]
-    return max(probabilities, key=lambda x: x[0])
+    except Exception as e:
+        text = f"An unexpected error occured in _max_temp_clothing: {str(e)}"
+        logger.exception(text)
+        raise Exception(text)
 
 
-def _cloth_for_weather(prob: int, weather: str) -> str:
+def _get_highest_prob_weather(day: Dict[str, str]) -> Tuple[int, str]:
+    try:
+        day_rain_prob = day["Day"]["RainProbability"]
+        night_rain_prob = day["Night"]["RainProbability"]
+        day_snow_prob = day["Day"]["SnowProbability"]
+        night_snow_prob = day["Night"]["SnowProbability"]
+        day_ice_prob = day["Day"]["IceProbability"]
+        night_ice_prob = day["Night"]["IceProbability"]
+        probabilities = [
+            (max(day_rain_prob, night_rain_prob), "rain"),
+            (max(day_snow_prob, night_snow_prob), "snow"),
+            (max(day_ice_prob, night_ice_prob), "ice"),
+        ]
+        return max(probabilities, key=lambda x: x[0])
+
+    except Exception as e:
+        text = f"An unexpected error occured in _get_highest_prob_weather {str(e)}"
+        logger.exception(text)
+        raise Exception(text)
+
+
+def _clothing_from_weather(prob: int, weather: str) -> str:
+    cloth = ""
     if weather == "rain":
-        if prob > 50:
-            return "Rain Coat"
+        cloth = "Rain Coat"
+    elif weather == "snow":
+        cloth = "Snow Outfit"
+    elif weather == "ice":
+        cloth = "Shell Jacket"
 
-    if weather == "snow":
-        if prob > 50:
-            return "Snow Outfit"
-
-    if weather == "ice":
-        if prob > 50:
-            return "Shell Jacket"
+    return cloth if prob > 50 else None
 
 
 def _format_response(
-    date: str, max_temp_cloth: Union[str, Tuple[str, str]], cloth_weather: str
+    date: str, max_temp_cloth: str, weather_clothing: str
 ) -> Dict[str, List[str]]:
     cloths = []
     if isinstance(max_temp_cloth, tuple):
         cloths.extend(max_temp_cloth)
     else:
         cloths.append(max_temp_cloth)
-    cloths.append(cloth_weather)
+
+    if weather_clothing:
+        cloths.append(weather_clothing)
 
     return {"date": date, "cloths": cloths}
 
 
 if __name__ == "__main__":
-    parser = get_arg_parser()
+    parser = get_parse_args()
     args = parser.parse_args()
     ans = main(args.n_days, args.city_code)
     print(ans)
